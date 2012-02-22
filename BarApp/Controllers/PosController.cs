331a5fb523c692cd57d7858b6b03bab8d7c2eb31
ecnalyr <ecnalyr@gmail.com>
2012-02-21 19:38:07 -0600

    Added the ability to check if a purchase code exists via the POS link.
    Purchase an item, get a code, input the code in the text field seen at the POS page.  If the code exists in the database the screen will display the email associated with the order.
    
    Signed-off-by: ecnalyr <ecnalyr@gmail.com>

--- BarApp/Controllers/PosController.cs
+++ BarApp/Controllers/PosController.cs
@@ -19,5 +19,30 @@ namespace BarApp.Controllers
             return View();
         }
 
+        //
+        // POST: /Pos/
+
+        [HttpPost]
+        public ActionResult Index(FormCollection sampleKey)
+        {
+            string code = sampleKey["sampleCode"]; // Need to check if this key is active
+            // search for code in Order table
+            // Check if that order is "Active"
+            // explain the results
+            BarAppEntities orderDB = new BarAppEntities();
+            var order = orderDB.Orders.SingleOrDefault(
+                o => o.OrderCode == code
+                    && o.Active == true);
+            if (order == null)
+            {
+                ViewBag.sample = "null";
+            }
+            else
+            {
+                ViewBag.sample = order.Email;
+            }
+            return View();
+        }
+
     }
 }
